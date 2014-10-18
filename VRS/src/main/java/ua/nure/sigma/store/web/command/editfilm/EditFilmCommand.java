75a08da4d40835e651b71271dc8b55f8b0aab7bd
Vlad Samotskiy <vlad-front@ukr.net>
2014-10-18 14:17:47 +0300

    edit film fields done

--- VRS/src/main/java/ua/nure/sigma/store/web/command/editfilm/EditFilmCommand.java
+++ VRS/src/main/java/ua/nure/sigma/store/web/command/editfilm/EditFilmCommand.java
@@ -16,43 +16,45 @@ import java.util.List;
  */
 public class EditFilmCommand extends Command implements IComplexCommand {
 
-    static final String FILMID_PARAM_NAME = "filmId";
-    static final String REMOVE_PARAM_NAME = "remove";
-    static final String FILM_TITLE_PARAM_NAME = "filmTitle";
-    static final String FILM_CATEGORIES_PARAM_NAME = "categoryName";
-    static final String FILM_AMOUNT_PARAM_NAME = "amount";
-    static final String FILM_DESCRIPTION_PARAM_NAME = "description";
-    static final String FILM_GENERAL_PRICE_PARAM_NAME = "generalPrice";
-    static final String FILM_RENT_PRICE_PARAM_NAME = "rentPrice";
-    static final String FILM_BONUS_PARAM_NAME = "bonus";
-    static final String FILM_YEAR_PARAM_NAME = "year";
-    static final String REQUEST_PARAM_NAME = "get";
-    static final String POST_RESULT = "controller?command=editFilm&get=true";
+	static final String FILMID_PARAM_NAME = "filmId";
+	static final String REMOVE_PARAM_NAME = "remove";
+	static final String FILM_TITLE_PARAM_NAME = "filmTitle";
+	static final String FILM_CATEGORIES_PARAM_NAME = "categoryName";
+	static final String FILM_AMOUNT_PARAM_NAME = "amount";
+	static final String FILM_DESCRIPTION_PARAM_NAME = "description";
+	static final String FILM_GENERAL_PRICE_PARAM_NAME = "generalPrice";
+	static final String FILM_RENT_PRICE_PARAM_NAME = "rentPrice";
+	static final String FILM_BONUS_PARAM_NAME = "bonus";
+	static final String FILM_YEAR_PARAM_NAME = "year";
+	static final String REQUEST_PARAM_NAME = "get";
+	static final String POST_RESULT = "controller?command=fullFilmList";
 
-    List<Command> commandsListeners;
+	List<Command> commandsListeners;
 
-    EditFilmCommand() {
-        commandsListeners = new ArrayList<Command>();
-    }
+	EditFilmCommand() {
+		commandsListeners = new ArrayList<Command>();
+	}
 
-    @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        notifyAllCommands(request, response);
-        String get = request.getParameter(EditFilmCommand.REQUEST_PARAM_NAME);
-        if (get != null && get.equals("true")) {
-            return Paths.PAGE_EDIT_FILM;
-        }
-        return POST_RESULT;
-    }
+	@Override
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		notifyAllCommands(request, response);
+		String get = request.getParameter(EditFilmCommand.REQUEST_PARAM_NAME);
+		if (get != null && get.equals("true")) {
+			return Paths.COMMAND_FULL_FILM_LIST;
+		}
+		return POST_RESULT;
+	}
 
-    private void notifyAllCommands(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        for (Command command : commandsListeners) {
-            command.execute(request, response);
-        }
-    }
+	private void notifyAllCommands(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		for (Command command : commandsListeners) {
+			command.execute(request, response);
+		}
+	}
 
-    @Override
-    public void addCommandListener(Command command) {
-        commandsListeners.add(command);
-    }
+	@Override
+	public void addCommandListener(Command command) {
+		commandsListeners.add(command);
+	}
 }
--- VRS/src/main/java/ua/nure/sigma/store/web/command/editfilm/EditFilmFillCommand.java
+++ VRS/src/main/java/ua/nure/sigma/store/web/command/editfilm/EditFilmFillCommand.java
@@ -1,5 +1,6 @@
 package ua.nure.sigma.store.web.command.editfilm;
 
+import ua.nure.sigma.store.dao.DAOFactory;
 import ua.nure.sigma.store.entity.Film;
 import ua.nure.sigma.store.web.command.Command;
 
@@ -9,16 +10,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Created by Сергей on 18.10.14.
+ * Created by Vlad Samotskiy on 18.10.14.
  */
 public class EditFilmFillCommand extends Command {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String filmID = request.getParameter("id");
-        //getting from dao film object
+        String filmIDString = request.getParameter(EditFilmCommand.FILMID_PARAM_NAME);
+        int filmId = 0;
         Film film = new Film();
-
+        try {
+            if (filmIDString != null) {
+                filmId = Integer.parseInt(filmIDString);
+                film = DAOFactory.getInstance().getFilmDAO().findFilmByID(filmId);            
+            }
         request.getSession().setAttribute("editFilmObject", film);
+        } catch (Exception e) {
+        }
         return null;
     }
 }
