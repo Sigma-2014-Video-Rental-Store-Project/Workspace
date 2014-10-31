4574113209775771043276f6228d80232343a3da
Maxym Sinkevich <maksim.sinkevich20@gmail.com>
2014-10-31 20:43:30 +0200

    sort for customer details

---
+++ VRS/src/main/java/ua/nure/sigma/store/comparators/CustomerDetailsComparatorFactory.java
@@ -0,0 +1,35 @@
+package ua.nure.sigma.store.comparators;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Maksim Sinkevich on 31.10.2014.
+ */
+public class CustomerDetailsComparatorFactory {
+    public static final String TITLE_COMPARATOR = "name";
+    public static final String DAYS_LEFT_COMPARATOR = "daysLeft";
+    public static final String END_DATE_COMPARATOR = "endDate";
+    public static final String START_DATE_COMPARATOR = "startDate";
+
+    private static Map<String, Comparator> comparators;
+
+    static {
+        comparators = new HashMap<String, Comparator>();
+        comparators.put(TITLE_COMPARATOR, new CustomerDetailsTitleComparator());
+        comparators.put(DAYS_LEFT_COMPARATOR, new CustomerDetailsDaysLeftComparator());
+        comparators.put(END_DATE_COMPARATOR, new CustomerDetailsEndDateComparator());
+        comparators.put(START_DATE_COMPARATOR, new CustomerDetailsStartDateComparator());
+    }
+
+    /**
+     * This function returns the comparator by sorting name.
+     *
+     * @param sortName may be "name", "price" or something else. You can get it from static fields of this class
+     * @return Comparator that can compare films by name of sorting
+     */
+    public static Comparator getComparator(String sortName) {
+        return comparators.get(sortName);
+    }
+}
