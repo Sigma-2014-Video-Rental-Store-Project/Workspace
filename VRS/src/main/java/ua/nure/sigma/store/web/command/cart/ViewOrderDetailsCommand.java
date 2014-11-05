b75056776c4c7dc27f3d24fe82ea51644e72881d
nikolaienko <vlad.nikolaienko@gmail.com>
2014-11-06 00:38:22 +0200

    almost done mapper

--- VRS/src/main/java/ua/nure/sigma/store/web/command/cart/ViewOrderDetailsCommand.java
+++ VRS/src/main/java/ua/nure/sigma/store/web/command/cart/ViewOrderDetailsCommand.java
@@ -1,8 +1,9 @@
 package ua.nure.sigma.store.web.command.cart;
 
 import org.apache.log4j.Logger;
-import ua.nure.sigma.store.entity.OrderDetailsView;
-import ua.nure.sigma.store.entity.Rent;
+import ua.nure.sigma.store.dao.DAOFactory;
+import ua.nure.sigma.store.dao.FilmDAO;
+import ua.nure.sigma.store.entity.*;
 import ua.nure.sigma.store.web.Paths;
 import ua.nure.sigma.store.web.command.Command;
 
@@ -10,6 +11,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vlad on 04.11.14.
@@ -31,8 +34,33 @@ public class ViewOrderDetailsCommand extends Command{
         return Paths.PAGE_VIEW_ORDER_DETAIL;
     }
 
-    private OrderDetailsView mapRentToOrderDetailsView(Rent rent){
+    private OrderDetailsView mapRentToOrderDetailsView(Rent rent) {
+        List<RentedFilmView> filmList = mapFilmForRentToRentedFilmView(rent.getFilmList());
+        Customer customer = DAOFactory.getInstance().getCustomerDAO().findCustomerByID(rent.getCustomerID());
+        return map(rent,filmList, customer);
+    }
+
+    private OrderDetailsView map(Rent rent, List<RentedFilmView> films, Customer customer){
         ///TODO: mapper
-        return new OrderDetailsView(0,null,null,null,0,0,0);
+        return new OrderDetailsView(0, null, null, null, 0, 0, 0);
+    }
+    private List<RentedFilmView> mapFilmForRentToRentedFilmView(List<FilmForRent> forRent){
+        FilmDAO filmDAO = DAOFactory.getInstance().getFilmDAO();
+        List<RentedFilmView> filmList = new ArrayList<RentedFilmView>();
+        for (FilmForRent filmForRent : forRent){
+            Film film = filmDAO.findFilmById(filmForRent.getFilmID());
+            filmList.add(mapToRentedFilmView(film, filmForRent));
+        }
+        return filmList;
+    }
+    private RentedFilmView mapToRentedFilmView(Film film, FilmForRent forRent){
+
+        return new RentedFilmView(
+                        film.getTitle(),
+                        forRent.getCopies(),
+                        forRent.getFutureDate(),
+                        film.getRentPrice()
+        );
     }
 }
+
