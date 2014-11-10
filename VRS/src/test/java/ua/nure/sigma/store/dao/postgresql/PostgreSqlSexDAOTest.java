ead42c8d040e1930d35af65596b5044af83d51c8
Denys Shevchenko <shevden20@gmail.com>
2014-11-10 21:40:21 +0200

    Admin list bug fix.

--- VRS/src/test/java/ua/nure/sigma/store/dao/postgresql/PostgreSqlSexDAOTest.java
+++ VRS/src/test/java/ua/nure/sigma/store/dao/postgresql/PostgreSqlSexDAOTest.java
@@ -15,18 +15,4 @@ public class PostgreSqlSexDAOTest {
         dao = DAOFactory.getInstance().getSexDAO();
     }
 
-    @Test
-    public void testFindSexByID() throws Exception {
-        assertNotNull(dao.findSexByID(1,1));
-    }
-
-    @Test
-    public void testFindAllSex() throws Exception {
-        assertNotEquals(dao.findAllSex(1).size(), 0);
-    }
-
-    @Test
-    public void testFindSexIDBySexName() throws Exception {
-        assertNotNull(dao.findSexIDBySexName("male"));
-    }
 }
