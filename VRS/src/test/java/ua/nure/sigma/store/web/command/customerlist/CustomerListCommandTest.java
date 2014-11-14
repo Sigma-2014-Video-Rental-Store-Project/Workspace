a14774cdda0fbbc4f6e2679a11803106a4c1ced7
Sergey Laposhko <sergeylaposhko@gmail.com>
2014-11-14 17:04:15 +0200

    CustomerListCommand test fix

--- VRS/src/test/java/ua/nure/sigma/store/web/command/customerlist/CustomerListCommandTest.java
+++ VRS/src/test/java/ua/nure/sigma/store/web/command/customerlist/CustomerListCommandTest.java
@@ -8,39 +8,4 @@ import org.junit.Test;
  * @author Sergey Laposhko
  */
 public class CustomerListCommandTest {
-
-    /**
-     * Test execute.
-     *
-     * @throws Exception the exception
-     */
-    @Test
-    public void testExecute() throws Exception {
-	throw new RuntimeException("not yet implemented");
-    }
-
-    /**
-     * Test customer list command.
-     *
-     * @throws Exception the exception
-     */
-    @Test
-    public void testCustomerListCommand() throws Exception {
-	throw new RuntimeException("not yet implemented");
-    }
-
-    /**
-     * Test add command listener.
-     *
-     * @throws Exception the exception
-     */
-    @Test
-    public void testAddCommandListener() throws Exception {
-	throw new RuntimeException("not yet implemented");
-    }
-
-}
-
-class A {
-    
 }
